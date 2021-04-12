1800463b4425bf375064995867799edb3ebe087d
5joono <5joono@naver.com>
2021-04-12 14:47:58 +0900

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -1,6 +1,12 @@
 import torch
 import torch.nn as nn
-from torchsummary import summary
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+torch.cuda.is_available()
+cuda = torch.device('cuda')
 
 class bottle(nn.Module):
     def __init__(self, in_channels, mid_channels, out_channels):
@@ -44,7 +50,7 @@ class bottleTrans(nn.Module):
         self.conv1 = nn.Conv2d(in_channels, mid_channels, 1)
         self.bn1 = nn.BatchNorm2d(mid_channels)
         self.relu1 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(mid_channels, mid_channels, 3, padding=1)
+        self.trans = MHSA(mid_channels, mid_channels)
         self.bn2 = nn.BatchNorm2d(mid_channels)
         self.relu2 = nn.ReLU(inplace=True)
         self.conv3 = nn.Conv2d(mid_channels, out_channels, 1)
@@ -63,7 +69,7 @@ class bottleTrans(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu1(x)
-        x = self.conv2(x)
+        x = self.trans(x)
         x = self.bn2(x)
         x = self.relu2(x)
         x = self.conv3(x)
@@ -73,7 +79,15 @@ class bottleTrans(nn.Module):
         x = x + x0
         x = self.relu3(x)
         return x
-    
+
+class MHSA(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(MHSA, self).__init__()
+        self.layer1 = nn.Identity()
+
+    def forward(self, x):
+        x = self.layer1(x)
+        return x
         
 class botnet(nn.Module):
     def __init__(self):
@@ -108,7 +122,7 @@ class botnet(nn.Module):
             bottleTrans(2048, 512, 2048)
         )
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(2048, 1000)
+        self.fc = nn.Linear(2048, 10)
     
     def forward(self,x0):
         x = self.conv1(x0)
@@ -122,7 +136,92 @@ class botnet(nn.Module):
         x = self.multibottle4(x)
 
         x = self.avgpool(x)
+        x = torch.flatten(x,1)
         x = self.fc(x)
         return x
 
-mynet = botnet()
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+net = botnet().cuda()
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data[0].cuda(), data[1].cuda()
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].cuda(), data[1].cuda()
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        inputs, labels = data[0].cuda(), data[1].cuda()
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(10):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
